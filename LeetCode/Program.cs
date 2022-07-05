5e285b7825916206081a22929e0b0da689a67d4c
Alexandre <alexandre.trapp@hotmail.com>
2022-07-04 21:55:14 -0300

    add TwoSumArray exercise

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,34 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LeetCode;
 
+var result = TwoSumArray.TwoSum(
+    nums: new int[3] {3,2,3}, 
+    target: 6);
 
-var result = TwoSum(new int[3] {3,2,3}, 6);
-
-foreach (var num in result)
-    Console.WriteLine(num + ",");
-
-int[] TwoSum(int[] nums, int target)
-{
-
-    var result = new List<int>();
-
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int n = 0; n < nums.Length; n++)
-        {
-            if (i == n)
-                continue;
-
-            int sum = nums[i] + nums[n];
-            if (sum == target)
-            {
-                result.Add(i);
-                result.Add(n);
-
-                return result.ToArray();
-            }
-        }
-    }
-
-    return result.ToArray();
-}
+Console.WriteLine(string.Join(',', result));
---
+++ LeetCode/TwoSumArray.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode;
+
+public static class TwoSumArray
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var result = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int n = 0; n < nums.Length; n++)
+            {
+                if (i == n)
+                    continue;
+
+                int sum = nums[i] + nums[n];
+                if (sum == target)
+                {
+                    result.Add(i);
+                    result.Add(n);
+
+                    return result.ToArray();
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}
