4d9f022b75c7a33ba58c0e0a4c45b2c5c50fb9ee
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2022-07-05 19:59:19 -0300

    add other examples

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,8 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCode;
 
-var result = TwoSumArray.TwoSum(
-    nums: new int[3] {3,2,3}, 
+var result = TwoSumArray.TwoSumBruteForce(
+    nums: new int[3] { 3, 2, 3 },
     target: 6);
 
-Console.WriteLine(string.Join(',', result));
+var result1 = TwoSumArray.TwoSumHashMap1(
+    nums: new int[4] { 1, 2, 3, 4 },
+    target: 7);
+
+var result2 = TwoSumArray.TwoSumHashMap2(
+    nums: new int[6] { 3, 2, 1, 8, 6, 9 },
+    target: 15);
+
+var result3 = TwoSumArray.TwoSumHashMap3(
+    nums: new int[5] { 8, 6, 8, 1, 3 },
+    target: 9);
+
+var result4 = TwoSumArray.TwoSumHashMap4(
+    nums: new int[4] { 8, 9, 7, 6 },
+    target: 14);
+
+var result5 = TwoSumArray.TwoSumHashMap5(
+    nums: new int[5] { 9, 5, 8, 2, 4 },
+    target: 10);
+
+Console.WriteLine("TwoSumBruteForce: " + string.Join(',', result));
+Console.WriteLine("TwoSumHashMap1: " + string.Join(',', result1));
+Console.WriteLine("TwoSumHashMap2: " + string.Join(',', result2));
+Console.WriteLine("TwoSumHashMap3: " + string.Join(',', result3));
+Console.WriteLine("TwoSumHashMap4: " + string.Join(',', result4));
+Console.WriteLine("TwoSumHashMap5: " + string.Join(',', result5));
--- LeetCode/TwoSumArray.cs
+++ LeetCode/TwoSumArray.cs
@@ -2,7 +2,7 @@
 
 public static class TwoSumArray
 {
-    public static int[] TwoSum(int[] nums, int target)
+    public static int[] TwoSumBruteForce(int[] nums, int target)
     {
         var result = new List<int>();
 
@@ -25,5 +25,91 @@ public static class TwoSumArray
         }
 
         return result.ToArray();
+    }
+
+    public static int[] TwoSumHashMap1(int[] nums, int target)
+    {
+        var items = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (items.ContainsKey(target - nums[i]))
+                return new int[] { i, items[target - nums[i]] };
+
+            else if (!items.ContainsKey(nums[i]))
+                items.Add(nums[i], i);
+        }
+
+        return new int[2];
+    }
+
+    public static int[] TwoSumHashMap2(int[] nums, int target)
+    {
+        var resultDict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+            if (!resultDict.ContainsKey(nums[i]))
+                resultDict.Add(nums[i], i);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (resultDict.ContainsKey(complement) && resultDict[complement] != i)
+                return new int[] { i, resultDict[complement] };
+        }
+
+        return new int[2];
+    }
+
+    public static int[] TwoSumHashMap3(int[] nums, int target)
+    {
+        var resultDict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int resultNum = target - nums[i];
+
+            if (resultDict.ContainsKey(resultNum))
+            {
+                var result = new int[] { resultDict[resultNum], i };
+                return result;
+            }
+
+            if (!resultDict.ContainsKey(nums[i]))
+                resultDict.Add(nums[i], i);
+        }
+        return new int[2];
+    }
+
+    public static int[] TwoSumHashMap4(int[] nums, int target)
+    {
+        var complement = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (complement.ContainsKey(nums[i]))
+                return new int[] { complement[nums[i]], i };
+
+            else if (!complement.ContainsKey(target - nums[i]))
+                complement.Add(target - nums[i], i);
+        }
+
+        return new int[2];
+    }
+
+    public static int[] TwoSumHashMap5(int[] nums, int target)
+    {
+        var items = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (items.ContainsKey(target - nums[i]))
+                return new int[] { i, items[target - nums[i]] };
+
+            else if (!items.ContainsKey(nums[i]))
+                items.Add(nums[i], i);
+        }
+
+        return new int[2];
     }
 }
