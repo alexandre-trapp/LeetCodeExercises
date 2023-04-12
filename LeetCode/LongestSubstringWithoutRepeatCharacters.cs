59f51da9fb165c7eeb088d8d8138ea5e5268d693
Alexandre Cordeiro Trapp <42077703+alexandre-trapp@users.noreply.github.com>
2023-04-11 22:23:23 -0300

    add LongestSubstringWithoutRepeatCharacters

---
+++ LeetCode/LongestSubstringWithoutRepeatCharacters.cs
@@ -0,0 +1,15 @@
+public class LongestSubstringWithoutRepeatCharacters 
+{
+    public int LengthOfLongestSubstring(string s) 
+    {
+        var charSet = new HashSet<char>();
+
+        int left=0, right=0, maxLength=0;
+
+        while (right < s.Length) 
+        {
+            if (!charSet)    
+        } 
+
+    }
+}
