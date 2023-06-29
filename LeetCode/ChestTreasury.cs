d8c923424ef25f65c5ce44f02fdd5c2bf6020340
Alexandre <alexandre.trapp@hotmail.com>
2023-06-28 22:59:22 -0300

    add chestTreasury code

---
+++ LeetCode/ChestTreasury.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    public static class ChestTreasury
+    {
+        public static int GetResult(int[] chests, int t)
+        {
+            int chestsSize = chests.Length;
+            int[,] maxTreasures = new int[chestsSize, t + 1];
+
+            for (int j = 1; j <= t; j++)
+            {
+                if (j >= 1)
+                {
+                    maxTreasures[0, j] = chests[0];
+                }
+            }
+
+            for (int i = 1; i < chestsSize; i++)
+            {
+                for (int j = 1; j <= t; j++)
+                {
+                    maxTreasures[i, j] = maxTreasures[i - 1, j];
+                    if (j >= i + 1)
+                    {
+                        maxTreasures[i, j] = Math.Max(maxTreasures[i, j], maxTreasures[i - 1, j - (i + 1)] + chests[i]);
+                    }
+                }
+            }
+
+            return maxTreasures[chestsSize - 1, t];
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCode;
+using System;
+
+// Chest treasury
+TestTreasuryChestCases();
+
+return;
 
 // Verify animals
 var animalsLength = OtherExercises.Animals("Elephant-Tiger-Lion-");
@@ -42,3 +48,52 @@ Console.WriteLine("TwoSumHashMap2: " + string.Join(',', result2));
 Console.WriteLine("TwoSumHashMap3: " + string.Join(',', result3));
 Console.WriteLine("TwoSumHashMap4: " + string.Join(',', result4));
 Console.WriteLine("TwoSumHashMap5: " + string.Join(',', result5));
+
+void TestTreasuryChestCases()
+{
+    int[][] testCases = new int[][] {
+        new int[] { 1, 4, 2 },
+        new int[] { 7, 8, 9 },
+        new int[] { 3, 5, 2, 6 },
+        new int[] { 2, 3, 1, 5, 4 },
+        new int[] { 9, 7, 6, 3 },
+        new int[] { 10, 5, 8, 2, 4 },
+        new int[] { 6, 4, 7, 3, 5 },
+        new int[] { 1, 2, 3, 4, 5 },
+        new int[] { 8, 9, 10, 7 },
+        new int[] { 2, 2, 2, 2, 2 }
+    };
+
+    int[] timeLimits = { 3, 2, 4, 5, 3, 6, 5, 6, 4, 5 };
+    int[] results = new int[10];
+
+    int[] expectedResults = { 5, 8, 11, 9, 18, 15, 18, 15, 18, 4 };
+
+    Console.Write("run algorithm...");
+
+    for (int i = 0; i < testCases.Length; i++)
+    {
+        Console.WriteLine("-------");
+
+        int[] chests = testCases[i];
+        int t = timeLimits[i];
+
+        int result = ChestTreasury.GetResult(chests, t);
+        results[i] = result;
+        Console.WriteLine($"Test Case {i + 1}: Chests = [{string.Join(", ", chests)}], Time Limit = {t}, Result = {result}");
+
+        bool equals = Assert.AreEqual(expectedResults[i], results[i]);
+        Console.WriteLine(equals ? "test passed" : "test failed!");
+        Console.WriteLine("-------");
+    }
+}
+
+
+public static class Assert
+{
+    public static bool AreEqual(int expected, int actual)
+    {
+        Console.WriteLine($"expected={expected}; actual={actual}");
+        return expected == actual;
+    }
+}
