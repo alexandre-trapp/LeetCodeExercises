69f8d2547a02d719562b6e8b11741e237792358f
alexandre-trapp <alexandre.trapp@hotmail.com>
2024-05-07 14:37:34 -0300

    add TopKFrequentElements

---
+++ LeetCode/TopKFrequentElements.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode
+{
+    public static class TopKFrequentElements
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            int[] result = new int[k];
+            var dictNums = new Dictionary<int, int>();
+                
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dictNums.ContainsKey(nums[i]))
+                {
+                    dictNums[nums[i]]++;
+                }
+                else
+                {
+                    dictNums.Add(nums[i], 1);
+                }
+            }
+
+            var pQueue = new PriorityQueue<int, int>();
+            foreach (var key in dictNums.Keys)
+            {
+                pQueue.Enqueue(key, dictNums[key]);
+                if (pQueue.Count > k) pQueue.Dequeue();
+            }
+
+            int i2 = k;
+            while (pQueue.Count > 0)
+            {
+                result[--i2] = pQueue.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
